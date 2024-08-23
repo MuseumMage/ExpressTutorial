ccd298fa77d97d5f8a8f02e2cb25c8a08c8e6251
zhangbo <mageboer@qq.com>
2024-08-23 16:30:35 +0800

    添加bookinstance表单

--- express-locallibrary-tutorial/controllers/bookinstanceController.js
+++ express-locallibrary-tutorial/controllers/bookinstanceController.js
@@ -1,4 +1,7 @@
-var BookInstance = require('../models/bookinstance');
+const Book = require("../models/book");
+const BookInstance = require('../models/bookinstance');
+const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 // Display list of all BookInstances
 exports.bookinstance_list = async function (req, res, next) {
@@ -30,15 +33,63 @@ exports.bookinstance_detail = async function (req, res, next) {
 };
 
 
-// Display BookInstance create form on GET
-exports.bookinstance_create_get = function (req, res, next) {
-  res.send('NOT IMPLEMENTED: BookInstance create GET');
-};
+// Display BookInstance create form on GET.
+exports.bookinstance_create_get = asyncHandler(async (req, res, next) => {
+  const allBooks = await Book.find({}, "title").sort({ title: 1 }).exec();
 
-// Handle BookInstance create on POST
-exports.bookinstance_create_post = function (req, res, next) {
-  res.send('NOT IMPLEMENTED: BookInstance create POST');
-};
+  res.render("bookinstance_form", {
+    title: "Create BookInstance",
+    book_list: allBooks,
+  });
+});
+
+// Handle BookInstance create on POST.
+exports.bookinstance_create_post = [
+  // Validate and sanitize fields.
+  body("book", "Book must be specified").trim().isLength({ min: 1 }).escape(),
+  body("imprint", "Imprint must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("status").escape(),
+  body("due_back", "Invalid date")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+
+  // Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a BookInstance object with escaped and trimmed data.
+    const bookInstance = new BookInstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors.
+      // Render form again with sanitized values and error messages.
+      const allBooks = await Book.find({}, "title").sort({ title: 1 }).exec();
+
+      res.render("bookinstance_form", {
+        title: "Create BookInstance",
+        book_list: allBooks,
+        selected_book: bookInstance.book._id,
+        errors: errors.array(),
+        bookinstance: bookInstance,
+      });
+      return;
+    } else {
+      // Data from form is valid
+      await bookInstance.save();
+      res.redirect(bookInstance.url);
+    }
+  }),
+];
 
 // Display BookInstance delete form on GET
 exports.bookinstance_delete_get = function (req, res, next) {
