9394c77dcca587fc8a3486a36cfcc8e5b224ee8c
zhangbo <mageboer@qq.com>
2024-08-23 11:40:05 +0800

    创建作者表单

--- express-locallibrary-tutorial/controllers/authorController.js
+++ express-locallibrary-tutorial/controllers/authorController.js
@@ -1,6 +1,7 @@
 const Author = require("../models/author");
 const Book = require("../models/book");
-// var async = require("async");
+const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require("express-validator");
 
 // 显示完整的作者列表
 exports.author_list = async function (req, res, next) {
@@ -38,15 +39,68 @@ exports.author_detail = async function (req, res, next) {
 };
 
 
-// 由 GET 显示创建作者的表单
-exports.author_create_get = (req, res) => {
-  res.send("未实现：作者创建表单的 GET");
+// Display Author create form on GET.
+exports.author_create_get = function (req, res, next) {
+  res.render("author_form", { title: "Create Author" });
 };
 
-// 由 POST 处理作者创建操作
-exports.author_create_post = (req, res) => {
-  res.send("未实现：创建作者的 POST");
-};
+// Handle Author create on POST.
+exports.author_create_post = [
+  // Validate and sanitize fields.
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified.")
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters."),
+  body("family_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Family name must be specified.")
+    .isAlphanumeric()
+    .withMessage("Family name has non-alphanumeric characters."),
+  body("date_of_birth", "Invalid date of birth")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+  body("date_of_death", "Invalid date of death")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+
+  // Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create Author object with escaped and trimmed data
+    const author = new Author({
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/errors messages.
+      res.render("author_form", {
+        title: "Create Author",
+        author: author,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+
+      // Save author.
+      await author.save();
+      // Redirect to new author record.
+      res.redirect(author.url);
+    }
+  }),
+];
 
 // 由 GET 显示删除作者的表单
 exports.author_delete_get = (req, res) => {
